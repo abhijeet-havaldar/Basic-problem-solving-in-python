f5900023a07e1a15e340a8fd4f0e6ea18aec627f
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-02-02 06:50:19 -0800

    Add files via upload

---
+++ practice_q19.py
@@ -0,0 +1,12 @@
+#Q :Write a Python program to get a newly-generated string from a given string where "Is" has been
+#  added to the front. Return the string unchanged if the given string already begins with "Is".
+
+def new_string(text):
+    if len(text) >= 2 and text[ :2] == "Is":
+        return text
+    else:
+        return "Is" + text
+    
+print(new_string("Array"))
+print(new_string("IsEmpty"))
+
