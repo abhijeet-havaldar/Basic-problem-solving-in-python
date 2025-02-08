f4eef06b02cdd5cf07bc23a633118ce0a9aa068a
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-02-08 06:57:15 -0800

    Add files via upload

---
+++ Build/practice_q37.py
@@ -0,0 +1,12 @@
+#Q:Write a Python program that displays your name, age, and address on three different lines.
+
+def personal_details():
+
+    name, age = "Abhi",19
+
+    address = "Maharashtra , sangli , india"
+
+    print("name : {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+
+personal_details()
