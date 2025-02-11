a176edf7f6a275e0640709e430159dce9d546ce7
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-02-11 07:45:35 -0800

    Add files via upload

---
+++ Build/practice_q46.py
@@ -0,0 +1,5 @@
+#Q:Write a Python program to retrieve the path and name of the file currently being executed.
+
+import os 
+
+print("current file name :",os.path.realpath(__file__))
---
+++ Build/practice_q47.py
@@ -0,0 +1,7 @@
+#Q:Write a Python program to find out the number of CPUs used.
+
+import multiprocessing 
+
+cpu_count = multiprocessing.cpu_count()
+
+print(cpu_count)
