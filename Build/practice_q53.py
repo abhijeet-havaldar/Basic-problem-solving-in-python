c2657492b4d1d29be610f85044db6024a6ccaa66
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-02-13 08:30:57 -0800

    Add files via upload

---
+++ Build/practice_q53.py
@@ -0,0 +1,11 @@
+#Q:Write a Python program to access environment variables.
+
+import os 
+
+print('*-----------------*')
+print(os.environ)
+print('*-----------------*')
+print(os.environ['HOME'])
+print('*-----------------*')
+print(os.environ['PATH'])
+print('*-----------------*')
