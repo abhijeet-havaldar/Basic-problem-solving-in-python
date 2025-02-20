4f846815b4c8ee6fde2f7ff180cf8d35cac2e120
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-02-20 06:08:29 -0800

    Add files via upload

---
+++ Build/practice_q76.py
@@ -0,0 +1,8 @@
+#Q:Write a Python program to get the command-line arguments (name of the  script, the number of arguments, arguments) passed to a script.
+
+
+import sys
+
+print("this is the name / path of the script"),sys.argv[0]
+print("Number of arguments:",len(sys.argv))
+print("Argument list:",str(sys.argv))
