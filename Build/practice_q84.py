8d78b0dfcb1e857d9b6ad45a68973e25908921fe
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-02-23 09:43:08 -0800

    Add files via upload

---
+++ Build/practice_q84.py
@@ -0,0 +1,8 @@
+#Q:Write a Python program to count the number of occurrences of a specific character in a string.
+
+s = "The quick brown fox jumps over the lazy dog"
+
+print("original string:")
+print(s)
+print("Number of occurrences of 'o' in the said strings:")
+print(s.count("o"))
