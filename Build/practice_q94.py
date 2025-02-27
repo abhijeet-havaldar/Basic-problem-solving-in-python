57373e73bb6be954188bca2d9a7cc6f25028e3e7
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-02-27 06:48:39 -0800

    Add files via upload

---
+++ Build/practice_q94.py
@@ -0,0 +1,6 @@
+x = b'Abc'
+
+print()
+print("convert bytes of the said string to a list of integers:")
+print(list(x))
+print()
---
+++ Build/practice_q95.py
@@ -0,0 +1,10 @@
+#Q:Write a Python program to check whether a string is numeric.
+
+str = 'a123'
+
+try:
+    i = float(str)
+except(ValueError,TypeError):
+    print('\nNot numeric')
+
+print()
