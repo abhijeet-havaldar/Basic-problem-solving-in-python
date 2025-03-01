775d663e79fee36e061dc2531437f5c569d4b4be
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-03-01 08:24:13 -0800

    Add files via upload

---
+++ Build/practice_q101.py
@@ -0,0 +1,16 @@
+#Q:Write a Python program to access and print a URL's content to the console.
+
+
+from http.client import HTTPConnection
+
+conn = HTTPConnection("example.com")
+
+conn.request("GET", "/")  
+
+
+result = conn.getresponse()
+contents = result.read()
+
+print(contents)
+
+conn.close()
---
+++ Build/practice_q102.py
@@ -0,0 +1,7 @@
+#Q:Write a Python program to get system command output.
+
+import subprocess
+returnend_next = subprocess.check_output("dir",shell=True,universal_newlines=True)
+
+print("dir command to list files and directories")
+print(returnend_next)
