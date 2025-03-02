9b473dee98cd85801d992534b59778c7386bf74f
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-03-02 06:34:15 -0800

    Delete practice_q106.py

--- practice_q106.py
+++
@@ -1,6 +0,0 @@
-#Q:Write a Python program to divide a path by the extension separator.
-
-import os.path
-
-for path in ['test.txt','filename','/user/system/test.txt ','/', ' ']:
-    print('"%s":'% path , os.path.splitext(path))
