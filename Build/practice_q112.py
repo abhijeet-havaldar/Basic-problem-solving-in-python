73a6a37d9921daa8d74a949151ae2fb90c4fe67c
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-03-06 06:31:59 -0800

    Add files via upload

---
+++ Build/practice_q112.py
@@ -0,0 +1,11 @@
+#Q:Write a Python program to remove the first item from a specified list.
+
+color = ["Red","Black","Green","White","Orange"]
+
+print("original list elements:")
+print(color)
+
+del color[0]
+print("After removing the first color:")
+print(color)
+print()
---
+++ Build/practice_q114.py
@@ -0,0 +1,8 @@
+#Q:Write a Python program to filter positive numbers from a list
+
+nums = [34,1,0,-23,12,-88]
+
+print("Original numbers in the list:",nums)
+
+new_list = list(filter(lambda x: x > 0, nums))
+print("Positive numbers in the said list:",new_list)
