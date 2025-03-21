a292ee49d9c5c9981ce6ad257c0bae65ab8dea15
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-03-21 10:05:42 -0700

    Add files via upload

---
+++ Build/practice_q147.py
@@ -0,0 +1,8 @@
+#Q:Write a Python function to check whether a number is divisible by another number. Accept two integer values from the user.
+
+def multipe(m,n):
+
+    return True if m % n == 0 else False
+
+print(multipe(20,5))
+print(multipe(7,2))
