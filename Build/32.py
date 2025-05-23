1b781be6fbea31c2cb0ef5abb182aa4f86f0f297
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-05-23 19:39:00 +0530

    Add files via upload

---
+++ Build/32.py
@@ -0,0 +1,11 @@
+#Q:Write a Python program to find the heights of the top three buildings in descending order from eight given buildings.
+
+print("Input the heights of eight buildings:")
+
+l = [int(input()) for i in range(8)]
+
+print("Heights of the top three buildings:")
+
+l = sorted(l)
+
+print(*l[:4:-1],sep='\n')
