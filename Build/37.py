f7314bf8d40531b4f4738a725cfd3306717eb2f5
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-05-30 19:42:37 +0530

    Add files via upload

---
+++ Build/37.py
@@ -0,0 +1,14 @@
+#Q:Write a Python program that reads an integer n and finds the number of combinations of a,b,c and d (0 = a,b,c,d = 9) where (a + b + c + d) will be equal to n.
+
+import itertools
+
+print("Input the number(n): ")
+
+n = int(input())
+
+result = 0
+
+for (i,j,k) in itertools.product(range(10),range(10),range(10)):
+    result += (0 <= n -(i + j + k) <= 9)
+
+print("Number of combinations:",result)
