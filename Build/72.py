866956401443115bdace848542ad202e97b2a330
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-08-19 20:04:42 +0530

    Add files via upload

---
+++ Build/72.py
@@ -0,0 +1,9 @@
+#Q:Write a Python program to check whether a given integer is a palindrome or not.
+
+def is_Palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
+print(is_Palindrome(100))
+print(is_Palindrome(252))
+print(is_Palindrome(-838))
