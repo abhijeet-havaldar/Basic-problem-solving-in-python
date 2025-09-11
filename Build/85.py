3d86275a9b30a3be551267ad01fc15ed291e93b6
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-09-11 21:07:15 +0530

    Add files via upload

---
+++ Build/85.py
@@ -0,0 +1,9 @@
+#Q:Write a Python program to check whether a given string is an "isogram" or not.
+
+def check_isogram(str1):
+    return len(str1) == len(set(str1.lower()))
+
+print(check_isogram("w3resource"))
+print(check_isogram("w3r"))
+print(check_isogram("Python"))
+print(check_isogram("Jawa"))
