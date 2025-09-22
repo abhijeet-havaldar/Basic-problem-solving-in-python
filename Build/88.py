644161391d72e8a7ed18420e4eac28d2c03f3419
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-09-22 22:04:19 +0530

    Add files via upload

---
+++ Build/88.py
@@ -0,0 +1,11 @@
+#Q:Write a Python program that accepts two strings and determines whether the letters in the second string are present in the first string
+
+def string_letter_check(str1, str2):
+    return all([char in str1.lower() for char in str2.lower()])
+
+
+print(string_letter_check("python", "ypth"))
+print(string_letter_check("python", "ypths"))   
+print(string_letter_check("python", "ypthon"))  
+print(string_letter_check("123456", "01234"))
+print(string_letter_check("123456", "1234"))
