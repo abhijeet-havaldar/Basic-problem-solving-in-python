935675a2256604379436f116a285ca37bdfa550a
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-09-27 21:54:40 +0530

    Add files via upload

---
+++ Build/92.py
@@ -0,0 +1,8 @@
+#Q:Write a Python program to compute the cumulative sum of numbers in a given list.
+
+def nums_camulative_sum(nums_list):
+    return [sum(nums_list[:i+1]) for i in range(len(nums_list))]
+
+print(nums_camulative_sum([10,20,30,40,50,60,7]))
+print(nums_camulative_sum([1,2,3,4,5]))
+print(nums_camulative_sum([0,1,2,3,4,5]))
