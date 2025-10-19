0be07936f836ad585f4d312b94abb06193317ea8
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-10-19 22:08:06 +0530

    Add files via upload

---
+++ Build/101.py
@@ -0,0 +1,9 @@
+#Q:Write a Python program to find the name of the oldest student in a given dictionary containing the names and ages of a group of students.
+
+def oldest_student(student):
+    return max(student, key=student.get)
+
+
+print(oldest_student({"Bernita Ahner": 12, "Kristie Marsico": 11, "Sara Pardee": 14, "Fallon Fabiano": 11, "Nidia Dominique": 15}))
+
+print(oldest_student({"Nilda Woodside": 12, "Jackelyn Pineda": 12.2, "Sofia Park": 12.4, "Joannie Archibald": 12.6, "Becki Saunder": 12.7}))
