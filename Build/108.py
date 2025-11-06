df254340d3ad7082d8fb4b4c7eaf7e8e4155297f
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-11-06 19:16:28 +0530

    Add files via upload

---
+++ Build/108.py
@@ -0,0 +1,14 @@
+#Q:Write a Python program that takes three integers and checks whether the sum of the last digit of the first number and the last digit of the second number equals the last digit of the third number.
+
+def check_last_digit(x,y,z):
+    return str(x + z)[-1] == str(y)[-1]
+
+print(check_last_digit(12, 26, 44))
+
+print(check_last_digit(145, 122, 1010))
+
+print(check_last_digit(0, 20, 40))
+
+print(check_last_digit(1, 22, 40))
+
+print(check_last_digit(145, 129, 104))
