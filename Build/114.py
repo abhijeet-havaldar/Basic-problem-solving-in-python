653d7c533df5cdbb8061e673b24f15f404ce0dd8
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-11-23 21:33:21 +0530

    Add files via upload

---
+++ Build/114.py
@@ -0,0 +1,13 @@
+#Q:Write a Python program to print letters from the English alphabet from a-z and A-Z.
+
+import string
+
+print("Alphabet from a-z:")
+for letter in string.ascii_lowercase:
+    print(letter, end=" ")
+
+print()
+
+print("alphabet form A-Z:")
+for letter in string.ascii_uppercase:
+    print(letter, end=" ")
