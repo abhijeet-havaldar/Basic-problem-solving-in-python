3e4cafd7f624d138b5a423ba355476c32c99fe2c
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-12-02 20:37:02 +0530

    Add files via upload

---
+++ Build/120.py
@@ -0,0 +1,20 @@
+#Q:Write a Python program to calculate Euclid's totient function for a given integer. Use a primitive method to calculate Euclid's totient function.
+
+def gcd(p,q):
+    while q != 0:
+        p, q = q, p % q
+    return p
+
+def is_coprime(x,y):
+    return gcd(x,y) == 1
+
+def phi_func(x):
+    if x == 1:
+        return 1
+    else:
+        n = [y for y in range(1,x) if is_coprime(x,y)]
+        return len(n)
+
+print(phi_func(10))
+print(phi_func(15))
+print(phi_func(33))
