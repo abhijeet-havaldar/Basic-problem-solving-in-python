f57ac5216342e5257277d129f7473e56b9e98574
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-12-03 19:46:18 +0530

    Add files via upload

---
+++ Build/121.py
@@ -0,0 +1,13 @@
+#Q:Write a Python program to create a coded string from a given string, using a specified formula.
+
+def test(input_str):
+    translated_str = input_str.translate(str.maketrans('PTSHA','90168'))
+    return translated_str
+
+str1 = "PHP"
+print("Original string:", str1)
+print("Coded string: ", test(str1))
+
+str2 = "JAVASCRIPT"
+print("\nOriginal string: ", str2)
+print("Coded string: ", test(str2))
