f714ce474216b0db709b209300df5fcb1d08c9a9
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-12-12 19:58:22 +0530

    Add files via upload

---
+++ Build/128.py
@@ -0,0 +1,18 @@
+#Q:Write a Python program to remove all vowels from a given string.
+
+import re
+
+def test(text):
+    return re.sub(r'[aeiou]+','', text, flags=re.IGNORECASE)
+
+text = "Python"
+print("Original string:", text)
+print("After removing all the vowels from the said string: " + test(text))
+
+text = "C Sharp"
+print("\nOriginal string:", text)
+print("After removing all the vowels from the said string: " + test(text))
+
+text = "JavaScript"
+print("\nOriginal string:", text)
+print("After removing all the vowels from the said string: " + test(text))
